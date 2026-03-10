4ae7da0f3b0227975a459e660662a72b8c42e95f
dankashay123 <niccomediate1@gmail.com>
2026-03-10 15:19:29 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'bubble-smash-v1';
+const ASSETS = [
+  '/bubble-smash/index.html',
+  '/bubble-smash/manifest.json',
+  '/bubble-smash/icon-192.png',
+  '/bubble-smash/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
